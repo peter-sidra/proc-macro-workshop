5c75bba7bf9c2bd98e21739fa026007b9f9e5747
peter-sidra <peter.sidra.18@ucl.ac.uk>
2022-03-05 18:38:58 +0200

    Passed all tests up to 5

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,13 +1,52 @@
-use proc_macro2::{Group, TokenStream, TokenTree};
+use proc_macro2::{Group, Punct, TokenStream, TokenTree};
 use quote::{format_ident, TokenStreamExt};
 use syn::{
     braced, parse::Parse, parse_macro_input, spanned::Spanned, Expr, ExprLit, ExprRange, Ident,
     Lit, RangeLimits, Token,
 };
 
-enum BodySection {
-    Repeated(TokenStream),
-    Normal(TokenStream),
+// Detects the repeat pattern #()*
+fn is_repeat_pattern(punct: &Punct, rest: &proc_macro2::token_stream::IntoIter) -> Option<Group> {
+    if punct.as_char() == '#' {
+        let mut iter = rest.clone();
+        if let Some(TokenTree::Group(group)) = iter.next() {
+            if matches!(iter.next(), Some(TokenTree::Punct(punct)) if punct.as_char() == '*') {
+                return Some(group);
+            }
+        }
+    }
+
+    None
+}
+
+// Detects the unified ident pattern [ident][~][ident]
+// returns the unified pattern and the number of consumed tokens
+fn is_unified_ident(
+    ident: &Ident,
+    repeat_ident: &Ident,
+    index: i32,
+    rest: &proc_macro2::token_stream::IntoIter,
+) -> Option<(Ident, usize)> {
+    let mut rest_clone = rest.clone();
+    if let Some(TokenTree::Punct(punct)) = rest_clone.next() {
+        if punct.as_char() == '~' {
+            if let Some(TokenTree::Ident(ref next_ident)) = rest_clone.next() {
+                if repeat_ident == next_ident {
+                    // TODO: check for a suffix (another [~][N]) pattern
+
+                    // We matched the [ident][~][N] pattern
+                    // Create new unified ident
+                    let literal = TokenTree::Literal(proc_macro2::Literal::i32_unsuffixed(index));
+                    let mut new_ident = format_ident!("{ident}{literal}");
+                    new_ident.set_span(repeat_ident.span());
+
+                    return Some((new_ident, 2));
+                }
+            }
+        }
+    }
+
+    None
 }
 
 #[derive(Debug)]
@@ -58,6 +97,24 @@ impl Parse for SeqRange {
 struct SeqInput {
     seq_range: SeqRange,
     body: TokenStream,
+    sectioned: bool,
+}
+
+impl SeqInput {
+    fn contains_repeat(body: TokenStream) -> bool {
+        let mut iter = body.into_iter();
+        while let Some(tt) = iter.next() {
+            let result = match tt {
+                TokenTree::Group(group) => Self::contains_repeat(group.stream()),
+                TokenTree::Punct(ref punct) => is_repeat_pattern(punct, &iter).is_some(),
+                _ => false,
+            };
+            if result == true {
+                return result;
+            }
+        }
+        false
+    }
 }
 
 impl Parse for SeqInput {
@@ -66,15 +123,21 @@ impl Parse for SeqInput {
 
         let body_ts;
         braced!(body_ts in input);
-        let body = body_ts.parse()?;
+        let body: TokenStream = body_ts.parse()?;
 
-        Ok(Self { seq_range, body })
+        let sectioned = Self::contains_repeat(body.clone());
+        Ok(Self {
+            seq_range,
+            body,
+            sectioned,
+        })
     }
 }
 
 struct Expander {
     ident: Ident,
     range: std::ops::Range<i32>,
+    sectioned: bool,
 }
 
 impl Expander {
@@ -99,29 +162,13 @@ impl Expander {
                     literal.set_span(ident.span());
                     return literal;
                 } else {
-                    let mut ts_iter_clone = ts_iter.clone();
-                    if let Some(TokenTree::Punct(punct)) = ts_iter_clone.next() {
-                        if punct.as_char() == '~' {
-                            if let Some(TokenTree::Ident(next_ident)) = ts_iter_clone.next() {
-                                if self.ident == next_ident {
-                                    // TODO: check for a suffix (another [~][N]) pattern
+                    if let Some((new_ident, num_of_consumed_tokens)) =
+                        is_unified_ident(ident, &self.ident, index, ts_iter)
+                    {
+                        // Advance the iterator past the consumed tokens
+                        ts_iter.nth(num_of_consumed_tokens - 1);
 
-                                    // We matched the [ident][~][N] pattern
-                                    // Create new unified ident
-                                    let literal = TokenTree::Literal(
-                                        proc_macro2::Literal::i32_unsuffixed(index),
-                                    );
-                                    let mut new_ident = format_ident!("{ident}{literal}");
-                                    new_ident.set_span(self.ident.span());
-
-                                    // Advance the original iter by 2
-                                    ts_iter.next();
-                                    ts_iter.next();
-
-                                    return TokenTree::Ident(new_ident);
-                                }
-                            }
-                        }
+                        return TokenTree::Ident(new_ident);
                     }
                 }
                 tt
@@ -149,6 +196,10 @@ impl Expander {
     }
 
     fn expand(&self, ts: TokenStream) -> TokenStream {
+        if !self.sectioned {
+            return self.expand_section(ts);
+        }
+
         let mut out_ts = TokenStream::new();
         let mut ts_iter = ts.into_iter();
 
@@ -163,20 +214,16 @@ impl Expander {
 
                     out_ts.append(TokenTree::Group(expanded_group));
                 }
-                TokenTree::Punct(ref punct) if punct.as_char() == '#' => {
-                    let mut ts_iter_clone = ts_iter.clone();
-                    if let Some(TokenTree::Group(group)) = ts_iter_clone.next() {
-                        if let Some(TokenTree::Punct(punct)) = ts_iter_clone.next() {
-                            if punct.as_char() == '*' {
-                                // Matched the repetition pattern
-                                out_ts.extend(self.expand_section(group.stream()));
+                TokenTree::Punct(ref punct) => {
+                    if let Some(group) = is_repeat_pattern(punct, &ts_iter) {
+                        // Matched the repetition pattern
+                        out_ts.extend(self.expand_section(group.stream()));
 
-                                // Advance the token stream iterator past the *
-                                ts_iter = ts_iter_clone;
+                        // Advance the token stream iterator past the *
+                        ts_iter.next();
+                        ts_iter.next();
 
-                                continue;
-                            }
-                        }
+                        continue;
                     }
                     out_ts.append(tt);
                 }
@@ -190,18 +237,20 @@ impl Expander {
 
 #[proc_macro]
 pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let SeqInput { seq_range, body } = parse_macro_input!(input as SeqInput);
-    // let seq_range = match syn::parse::<SeqRange>(input) {
-    //     Ok(sr) => sr,
-    //     Err(err) => return proc_macro::TokenStream::from(err.to_compile_error()),
-    // };
+    let SeqInput {
+        seq_range,
+        body,
+        sectioned,
+    } = parse_macro_input!(input as SeqInput);
 
     println!("body = {body}");
     println!("body = {:#?}", body);
+    println!("sectioned = {sectioned}");
 
     let expander = Expander {
         ident: seq_range.ident,
         range: seq_range.range,
+        sectioned,
     };
 
     let result = expander.expand(body);
